a1669ca321affac6c01eeb62e298bacb27827db0
Amanda Daley <amandabwi@gmail.com>
2017-04-07 13:30:30 -0400

    Add files via upload

---
+++ Document1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page Title</title>
+	<?php  
+try {  
+   $conn = new PDO( "sqlsrv:Server=(local);Database=AdventureWorks", NULL, NULL);   
+   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
+}  
+
+catch( PDOException $e ) {  
+   die( "Error connecting to SQL Server" );   
+}  
+
+echo "Connected to SQL Server\n";  
+
+$query = 'select * from Person.ContactType';   
+$stmt = $conn->query( $query );   
+while ( $row = $stmt->fetch( PDO::FETCH_ASSOC ) ){   
+   print_r( $row );   
+}  
+?>  
+
+</head>
+<body>
+
+
+
+</body>
+</html>
